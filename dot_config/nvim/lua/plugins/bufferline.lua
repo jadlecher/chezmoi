5ec01e04acacb13a1f25bc2b556626f11644b679
James Lecher <jadlecher@gmail.com>
2026-04-05 10:43:01 -0500

    nvim: hide terminal duplicate suffixes in bufferline

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -7,10 +7,27 @@ return {
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				name_formatter = function(tab)
+					local function strip_duplicate_suffix(name)
+						return (name or ""):gsub(" %[%d+%]$", "")
+					end
+
 					local bufnr = tab.bufnr or tab.buf
 					if bufnr and vim.api.nvim_buf_is_valid(bufnr) then
-						return vim.b[bufnr].terminal_name_display or tab.name
+						local display_name = vim.b[bufnr].terminal_name_display
+						if display_name and display_name ~= "" then
+							return strip_duplicate_suffix(display_name)
+						end
+
+						local bufname = vim.api.nvim_buf_get_name(bufnr)
+						if vim.startswith(bufname, "term:/") then
+							return strip_duplicate_suffix(bufname)
+						end
 					end
+
+					if vim.startswith(tab.name, "term:/") then
+						return strip_duplicate_suffix(tab.name)
+					end
+
 					return tab.name
 				end,
 			},
