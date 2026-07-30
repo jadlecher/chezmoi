d8cdb79e4aa2b84cc4e3fcbb572b561475201c18
James Lecher <jadlecher@gmail.com>
2026-07-29 20:41:41 -0500

    feat: add qutebrowser history navigation bindings

--- dot_config/private_qutebrowser/conf_d/bindings.py
+++ dot_config/private_qutebrowser/conf_d/bindings.py
@@ -4,3 +4,9 @@ from qutebrowser.config.config import ConfigContainer
 
 def apply(config: ConfigAPI, _: ConfigContainer):
     config.bind("<Ctrl-x><Ctrl-e>", "edit-command", mode="command")
+    config.bind(
+        "<Alt-j>", "completion-item-focus --history next", mode="command"
+    )
+    config.bind(
+        "<Alt-k>", "completion-item-focus --history prev", mode="command"
+    )
