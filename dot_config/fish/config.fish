131a74cdef51e8f8d2f62016e9e241e61b8f0c90
James Lecher <jadlecher@gmail.com>
2026-08-07 22:14:07 -0500

    Hide user and host from fish prompt

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
+    function prompt_login
+    end
+
     # Enable zoxide directory jumping when zoxide is installed.
     if type -q zoxide
         zoxide init fish | source
